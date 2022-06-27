a3f79b8888969846f09936488757d91715878b15
Tom Erik Støwer <testower@gmail.com>
2022-06-27 12:32:43 +0200

    Formatting

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
-import {App} from './App';
+import { App } from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
